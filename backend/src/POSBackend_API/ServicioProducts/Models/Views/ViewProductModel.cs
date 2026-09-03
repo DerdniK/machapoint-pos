78ae1661fd9c73fd6df62ae09dd99a7de1e911d6
Gustavo Solorzano Oseguera <gustavoaso26@gmail.com>
2026-09-02 22:14:13 -0600

    Feat: Create the new model for the product view from db

---
+++ backend/src/POSBackend_API/ServicioProducts/Models/Views/ViewProductModel.cs
@@ -0,0 +1,11 @@
+namespace ServicioProducts.Models.Views
+{
+    public class ViewProductModel
+    {
+        public int ProductId {get; set;}
+        public string Name {get; set;}
+        public string SKU {get; set;}
+        public double Price {get; set;}
+        public string ImageURL {get; set;}
+    }
+}
