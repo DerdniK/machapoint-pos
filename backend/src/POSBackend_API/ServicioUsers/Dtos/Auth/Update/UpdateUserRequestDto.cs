a3e891df9c8a2bd1f8582a9898389ad41a0ebe71
Gustavo Solorzano Oseguera <gustavoaso26@gmail.com>
2026-09-02 19:10:58 -0600

    Fix: fix the null exception

--- backend/src/POSBackend_API/ServicioUsers/Dtos/Auth/Update/UpdateUserRequestDto.cs
+++ backend/src/POSBackend_API/ServicioUsers/Dtos/Auth/Update/UpdateUserRequestDto.cs
@@ -7,6 +7,6 @@ namespace ServicioUsers.Dtos.Auth.Update
         public string? Password {get; set;}
         public string? Firstname {get; set;}
         public string? Lastname {get; set;}
-        public int Roleid {get; set;}
+        public int? Roleid {get; set;}
     }
 }
--- backend/src/POSBackend_API/ServicioUsers/Dtos/Auth/Update/UpdateUserResponseDto.cs
+++ backend/src/POSBackend_API/ServicioUsers/Dtos/Auth/Update/UpdateUserResponseDto.cs
@@ -4,9 +4,9 @@ namespace ServicioUsers.Dtos.Auth.Update
     {
         public bool Success {get; set;}
         public string Message {get; set;}
-        public string Username {get; set;} = "No modificado";
-        public string Firstname {get; set;} = "No modificado";
-        public string Lastname {get; set;} = "No modificado";
-        public int Roleid {get; set;}
+        public string? Username {get; set;} = "No modificado";
+        public string? Firstname {get; set;} = "No modificado";
+        public string? Lastname {get; set;} = "No modificado";
+        public int? Roleid {get; set;}
     }
 }
