8c7ef514446e6d6ca60d475a5a07575ffaa0b54f
Gustavo Solorzano Oseguera <gustavoaso26@gmail.com>
2026-09-02 20:37:43 -0600

    Feat: Create Product Response Dto

---
+++ backend/src/POSBackend_API/ServicioProducts/Dtos/Create/CreateProductResponseDto.cs
@@ -0,0 +1,9 @@
+namespace ServicioProducts.Dtos.Create
+{
+    public class CreateProductResponseDto
+    {
+        public bool Success {get; set;}
+        public string Message {get; set;}
+        public int Productid {get; set;}
+    }
+}
