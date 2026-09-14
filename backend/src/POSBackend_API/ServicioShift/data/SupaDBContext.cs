d460ee888dd9ebea143bf37d60e21d0b724de755
Gustavo Solorzano Oseguera <gustavoaso26@gmail.com>
2026-09-14 17:58:54 -0600

    feat: add the connection with the shift table

---
+++ backend/src/POSBackend_API/ServicioShift/data/SupaDBContext.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using ServicioShift.Models;
+
+namespace ServicioShift.Data
+{
+    public class SupaDBContext : DbContext
+    {
+        public SupaDBContext(DbContextOptions<SupaDBContext> options) : base(options) { } 
+        //^ "SupaDBContext" el nombre de la clase a la cual heredamos "DbContext"
+        public DbSet<Shift> ProductsTypeTable {get; set;}
+    }
+}
