2f4e7589ecc8a546ffbc66f6bf9fa9c9443dbcc3
Gustavo Solorzano Oseguera <gustavoaso26@gmail.com>
2026-09-14 17:31:03 -0600

    feat: add health for sales

---
+++ backend/src/POSBackend_API/ServicioSales/controller/SaleController.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ServicioSales.Dtos;
+using ServicioSales.Service;
+
+namespace ServicioSales.Controller
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class SaleController : ControllerBase
+    {
+        private readonly ISaleService _saleService;
+
+        public SaleController(ISaleService saleService)
+        {
+            _saleService = saleService;
+        }
+
+        [HttpGet("health")]
+        public IActionResult Health()
+        {
+            try
+            {
+                return Ok("Servicio Sales funcionando");
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, new 
+                { 
+                    message = "Ocurrió un error interno al registrar la venta.",
+                    detail = ex.Message 
+                });
+            }
+            
+        }
+
+        [Authorize]
+        [HttpPost]
+        public async Task<IActionResult> CreateSale(SaleRequestDto request)
+        {
+            try
+            {
+                var sale = await _saleService.CreateSaleAsync(request);
+                return StatusCode(StatusCodes.Status201Created, sale);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, new 
+                { 
+                    message = "Ocurrió un error interno al registrar la venta.",
+                    detail = ex.Message 
+                });
+            }
+        }
+
+    }
+}
