6244a5f5417146336ec5eda8d27c972f3406b719
Gustavo Solorzano Oseguera <gustavoaso26@gmail.com>
2026-09-14 18:37:28 -0600

    feat: add the shift service with the logic

---
+++ backend/src/POSBackend_API/ServicioShift/services/ShiftService.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Npgsql;
+using ServicioShift.Data;
+using ServicioShift.Dtos;
+using ServicioShift.Dtos.Close;
+using ServicioShift.Dtos.Open;
+
+namespace ServicioShift.Services
+{
+    public class ShiftService : IShiftService
+    {
+        public readonly SupaDBContext _context;
+        public ShiftService(SupaDBContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<ResponseShiftDto> OpenShiftAsync(OpenShiftDto request)
+        {
+            var sql = "SELECT sp_open_shift(@p_cashierid, @p_opening_amount)";
+
+            await _context.Database.ExecuteSqlRawAsync(sql,
+            new NpgsqlParameter("p_cashierid", request.CashierId),
+            new NpgsqlParameter("p_opening_amount", request.OpeningAmount)
+            );
+
+            return new ResponseShiftDto
+            {
+                Message = "Turno iniciado correctamente!",
+                Status = true,
+                ActualCash = request.OpeningAmount
+            };
+        }
+
+        public async Task<ResponseShiftDto> CloseShiftAsync(CloseShiftDto request)
+        {
+            var sql = "SELECT sp_close_shift_z_cut(@p_shiftid, @p_cashierid, @p_actual_cash, @p_notes)";
+
+            await _context.Database.ExecuteSqlRawAsync(sql,
+            new NpgsqlParameter("p_shiftid", request.ShiftId),
+            new NpgsqlParameter("p_cashierid", request.CashierId),
+            new NpgsqlParameter("p_actual_cash", request.ActualCash),
+            new NpgsqlParameter("p_notes", request.Notes)
+            );
+
+            return new ResponseShiftDto
+            {
+                Message = "Turno Cerrado correctamente!",
+                Status = true,
+                ShiftId = request.ShiftId,
+                CashierId = request.CashierId,
+                ActualCash = request.ActualCash,
+                Notes = request.Notes
+
+            };
+        }
+    }
+}
