f1c8fa1b7f388d757e5eb2cc547e6f7bffe5f36b
Gustavo Solorzano Oseguera <gustavoaso26@gmail.com>
2026-09-14 18:05:35 -0600

    feat: add model for table shifts

--- backend/src/POSBackend_API/ServicioShift/Models/ShiftModel.cs
+++ backend/src/POSBackend_API/ServicioShift/Models/ShiftModel.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,6 +9,19 @@ namespace ServicioShift.Models
 {
     public class Shift
     {
-        
+        [Key]
+        [Column("shiftid")]
+        public int ShiftId {get; set;}
+        [Column("cahierid")]
+        public Guid CashierId {get; set;}
+        [Column("opening_amount")]
+        public double Opening_amount {get; set;}
+        [Column("status")]
+        public string? Status {get; set;}
+        [Column("opened_at")]
+        public DateTime Opened_at {get; set;}
+        [Column("closed_at")]
+        public DateTime Closed_at {get; set;}
+
     }
 }
