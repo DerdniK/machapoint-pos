4e6d10d7893ae00d7291eb7ea63b8efd8c2a1f28
Gustavo Solorzano Oseguera <gustavoaso26@gmail.com>
2026-09-14 21:15:39 -0600

    feat: add the request and response dto from the zcut view

---
+++ backend/src/POSBackend_API/ServicioCut/Dtos/GetZCutRequestDto.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ServicioCut.Dtos
+{
+    public class GetZCutRequestDto
+    {
+        public int? ShiftId { get; set; }
+    }
+}
