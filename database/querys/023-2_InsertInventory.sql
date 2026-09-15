03593b0887b524fc48626717d7ee09dff2212282
Areli Jiménez <areli.eliza.jm@gmail.com>
2026-09-15 00:40:19 -0600

    feat: added provisionary values to inventory

---
+++ database/querys/023-2_InsertInventory.sql
@@ -0,0 +1,11 @@
+INSERT INTO inventory (productid, stock) VALUES
+(1, 25),   -- Vintage Poster
+(2, 50),  -- Metal Pin
+(3, 15),   -- Sticker Pack
+(4, 100), -- Holiday Postal Card
+(5, 30),   -- Art Poster
+(8, 8),    -- Posterchingon
+(9, 12),   -- Posterchingonsisimo
+(10, 2),   -- Postershin (Stock bajo para alertas)
+(11, 20),  -- asi123
+(12, 45); -- Postersini
